3e36221686ca3a3c378329000d0febbdf4ed5a2b
Bahadır Erdem <24529148+BeaSt2518@users.noreply.github.com>
2023-11-02 13:53:36 +0300

    I dont know what this is

---
+++ organize.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int a, b;
+    printf("Sayi giriniz: ");
+    scanf("%d" , a);
+    printf("Sayi giriniz 2: ");
+    scanf("%d" , b);
+    int result = a*b;
+    return result;
+}
