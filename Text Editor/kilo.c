745b5b8d0ac277fc219c0cdf2ac73c00e3def42d
Bahadır Erdem <24529148+BeaSt2518@users.noreply.github.com>
2024-06-20 15:04:43 +0300

    Turned off flags

--- Text Editor/kilo.c
+++ Text Editor/kilo.c
@@ -1,14 +1,36 @@
 #include <termios.h>
 #include <unistd.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct termios orig_termios;
+
+void disableRawMode() {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+} 
+
 void enableRawMode() {
-  struct termios raw;
-  tcgetattr(STDIN_FILENO, &raw);
-  raw.c_lflag &= ~(ECHO);
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
+  struct termios raw = orig_termios;
+  
+  raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+  raw.c_cflag |= (CS8);
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
+
 int main() {
   enableRawMode();
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+    if (iscntrl(c)){
+      printf("%d\r\n" , c);
+    }else {
+      printf("%d ('%c')\r\n" ,c , c);
+    }
+  }
   return 0;
 }
