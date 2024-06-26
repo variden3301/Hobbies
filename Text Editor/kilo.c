040549d7604dccfee5deeb8282ee3b613906f04c
Bahadır Erdem <24529148+BeaSt2518@users.noreply.github.com>
2024-06-26 10:17:26 +0300

    Fixed editordrawrows()

--- Text Editor/kilo.c
+++ Text Editor/kilo.c
@@ -214,38 +214,36 @@ void abFree(struct abuf *ab){
 }
 
 /* output */
-void editorDrawRows(struct abuf *ab){
-  int y;  
-  for (y = 0 ; y < E.screenrows; y++){
-    if(y >= E.numrows){
-      if (E.numrows == 0 && y == E.screenrows / 3){
+void editorDrawRows(struct abuf *ab) {
+  int y;
+  for (y = 0; y < E.screenrows; y++) {
+    if (y >= E.numrows) {
+      if (y == E.screenrows / 3 && E.numrows == 0) {
         char welcome[80];
-
         int welcomelen = snprintf(welcome, sizeof(welcome), "Kilo editor -- version %s", KILO_VERSION);
         if (welcomelen > E.screencols) welcomelen = E.screencols;
         int padding = (E.screencols - welcomelen) / 2;
-        if (padding){
+        if (padding) {
           abAppend(ab, "~", 1);
           padding--;
-        } 
+        }
         while (padding--) abAppend(ab, " ", 1);
         abAppend(ab, welcome, welcomelen);
-      }else{
+      } else {
         abAppend(ab, "~", 1);
       }
-    }else{
-      int len = E.row->size;
+    } else {
+      int len = E.row[y].size;  // Correctly access the current row
       if (len > E.screencols) len = E.screencols;
-      abAppend(ab, E.row->chars, len);
+      abAppend(ab, E.row[y].chars, len);  // Correctly access the current row
     }
     abAppend(ab, "\x1b[K", 3);
-    if (y < E.screenrows - 1)
-    {
+    if (y < E.screenrows - 1) {
       abAppend(ab, "\r\n", 2);
     }
-    
   }
 }
+
 
 void editorRefreshScreen(){
   struct abuf ab = ABUF_INIT;
