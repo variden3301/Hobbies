50cf408e89b9b5eab7162686edf68450196ccba4
Bahadır Erdem <24529148+BeaSt2518@users.noreply.github.com>
2025-07-10 02:45:11 +0300

    frmrr

---
+++ deneme.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "This is a placeholder for the C++ version of the password checker." << std::endl;
+    // The C++ code will implement similar functionality as the C code provided.
+    return 0;
+}
