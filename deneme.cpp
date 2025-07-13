38dcf3722f3afee5742e4f2901c76b5909bd997c
Bahadır Erdem <24529148+BeaSt2518@users.noreply.github.com>
2025-07-13 14:29:16 +0300

    Nothing really

--- deneme.cpp
+++ deneme.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "This is a placeholder for the C++ version of the password checker." << std::endl;
-    // The C++ code will implement similar functionality as the C code provided.
+    int doubleValue(10.9);
+    std::cout << "The value of doubleValue is: " << doubleValue << std::endl;
     return 0;
 }
